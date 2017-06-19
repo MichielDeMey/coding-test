e332b98d37bc1895ff86415e75ad45a2520c7dfb
Michiel De Mey <de.mey.michiel@gmail.com>
2017-06-19 14:14:24 +0200

    Improved testing command
    (Now works with dependencies)

--- problem-3/test.sh
+++ problem-3/test.sh
@@ -1,3 +1,2 @@
-docker run -v $PWD/server:/app \
-           --rm phpunit/phpunit \
-           --configuration phpunit.xml
+CONTAINER_ID=$(docker ps -qf "name=problem3_php")
+docker exec $CONTAINER_ID /var/www/teamleader/api/vendor/bin/phpunit
